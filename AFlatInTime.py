1e1d45196039be95d5987132e017d3076bdf0e44
matthew <natsdizon@yahoo.com>
2023-04-08 12:08:59 +0100

    added walk animation

--- AFlatInTime.py
+++ AFlatInTime.py
@@ -5,9 +5,9 @@ import random
 clock = pygame.time.Clock()
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load("Theme1.mp3")
-pygame.mixer.music.set_volume(0.1)
-pygame.mixer.music.play()
+#pygame.mixer.music.load("Theme1.mp3")
+#pygame.mixer.music.set_volume(0.1)
+#pygame.mixer.music.play()
 #pygame.mixer.music.fadeout(5000)
 BGCOLOUR=(88,159,223)
 screen=pygame.display.set_mode((480*2,320*2))
@@ -20,7 +20,7 @@ for file in os.listdir("sounds/jumps"):
     jumpsoundeffects.append(sfx)
 for file in os.listdir("sounds/doublejumps"):
     sfx=pygame.mixer.Sound("sounds/doublejumps/"+file)
-    sfx.set_volume(0.25)
+    sfx.set_volume(0.1)
     doublejumpsoundeffects.append(sfx)
 
 class GameSprite:
@@ -58,27 +58,38 @@ class Map(GameSprite):
 class HatKid(GameSprite):
     def __init__(self,filename):
         super().__init__(0,0,filename)
-        
-        self.walk1= pygame.image.load("sprite/HatKid/walk1.png")
-        self.walk2= pygame.image.load("sprite/HatKid/walk2.png")
-        self.standing= pygame.image.load("sprite/HatKid/standing.png")
-        self.prejump= pygame.image.load("sprite/HatKid/prejump.png")
-        self.fall= pygame.image.load("sprite/HatKid/fall.png")
+        self.walks=[]
+        for counter in range (1,5):
+            walk= pygame.image.load("sprite/HatKid/new_sprite/walk"+str(counter)+".png")
+            walk= pygame.transform.scale(walk,(64,64))
+            self.walks.append(walk)
+        self.idle= pygame.image.load("sprite/HatKid/new_sprite/idle.png")
+        self.idle=pygame.transform.scale(self.idle,(64,64))
+        self.direction="right"
+        self.x_speed = 0
+        #self.standing= pygame.image.load("sprite/HatKid/standing.png")
+        #self.prejump= pygame.image.load("sprite/HatKid/prejump.png")
+        #self.fall= pygame.image.load("sprite/HatKid/fall.png")
 
-        self.walk1= pygame.transform.scale(self.walk1,(52,52))
-        self.walk2= pygame.transform.scale(self.walk2,(52,52))
-        self.standing= pygame.transform.scale(self.standing,(52,52))
-        self.prejump= pygame.transform.scale(self.prejump,(52,52))
-        self.fall= pygame.transform.scale(self.fall,(52,52))
+        #self.standing= pygame.transform.scale(self.standing,(52,52))
+        #self.prejump= pygame.transform.scale(self.prejump,(52,52))
+        #self.fall= pygame.transform.scale(self.fall,(52,52))
         self.yspeed=0
         self.jumps=0
         self.canjump= True
-    def update(self,keyspressedlisted):
+        self.walk_index=0
 
-        
-        #if self.rect.colliderect(p1):
-        #    print ("collidead",p1.x,p1.y)
-        #print (self.x,self.y,"hatkid")
+
+    def walk(self):
+        if self.walk_index >= 4-1/15:
+            self.walk_index=0
+        else:
+            self.walk_index += 1/15
+        return int(self.walk_index)
+
+    def update(self,keyspressedlisted):
+    
+
         if self.yspeed <= 5:
             self.yspeed +=0.2
         else:
@@ -88,10 +99,9 @@ class HatKid(GameSprite):
             self.y=300
             self.jumps=0
 
-        
-        #time.sleep(0.2)
-        #screen.blit(self.walk2,(0,0))
-        #time.sleep(0.2)
+        for counter in range(4):
+            screen.blit(self.walks[counter],(counter*64,0))
+
         keyspressedlisted=pygame.key.get_pressed()
         if keyspressedlisted[pygame.K_w] or keyspressedlisted[pygame.K_SPACE]:
             if self.jumps <2 and self.canjump:
@@ -109,19 +119,51 @@ class HatKid(GameSprite):
         else:
             self.canjump=True
 
-        if keyspressedlisted[pygame.K_s]:
-            self.y +=3
+        #if keyspressedlisted[pygame.K_s]:
+            #self.y +=3
+        
 
         if keyspressedlisted[pygame.K_d]:
-            self.x +=3
+            if self.x_speed >= 3:
+                self.x_speed = 3
+            else:
+                self.x_speed+=0.1
+            self.x +=self.x_speed
 
-        if keyspressedlisted[pygame.K_a]:
-            self.x -=3
 
-        #print(self.yspeed,self.y,self.jumps,self.canjump)
+            self.direction="right"
+
+            screen.blit(self.walks[self.walk()],(self.x,self.y))
+        elif keyspressedlisted[pygame.K_a]:
+            if self.x_speed <= -3:
+                self.x_speed =- 3
+            else:
+                self.x_speed-=0.1
+            self.x +=self.x_speed
+
+            self.direction="left"
+            flipped=pygame.transform.flip(self.walks[self.walk()],True,False)
+            screen.blit(flipped,(self.x,self.y))
+        else:
+            self.swap()
+            # self.x_speed=self.x_speed*0.9
+            # if self.direction=="right":
+            #     screen.blit(self.idle,(self.x,self.y))
+            #     self.x += self.x_speed
+            # if self.direction=="left":
+            #     screen.blit(pygame.transform.flip(self.idle,True,False),(self.x,self.y))
+            #     self.x -= self.x_speed
         self.y+=self.yspeed
 
-        screen.blit(self.walk1,(self.x,self.y))
+    def swap(self):
+        self.x_speed=self.x_speed*0.9
+        if self.direction=="right":
+            screen.blit(self.idle,(self.x,self.y))
+            self.x += self.x_speed
+        if self.direction=="left":
+            screen.blit(pygame.transform.flip(self.idle,True,False),(self.x,self.y))
+            self.x += self.x_speed
+
 
 class Rectangle():
     def __init__(self,xcoord,ycoord,width,height):
@@ -149,7 +191,7 @@ while game:
     map1.update()
 
     p1=map1.platform1.get_rect()
-    hk=hatkid.rect
+    #hk=hatkid.rect
     #print(p1.x,p1.y,p1.height,p1.width)
     #print(hk.x,hk.y,hk.height,hk.width)
     hatkid.update(keyspressedlisted)
